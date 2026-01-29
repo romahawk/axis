604b927ec5bb67b485832047779cda9bd460b67b
Roman Mazuryk <romazuryk@gmail.com>
2026-01-29 11:57:26 +0100

    Update TodayTop3Panel.tsx

--- frontend/src/features/dashboard/todayTop3/TodayTop3Panel.tsx
+++ frontend/src/features/dashboard/todayTop3/TodayTop3Panel.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { useQueryClient } from "@tanstack/react-query";
-import { ChevronDown } from "lucide-react";
 
 import { Panel } from "../../../components/Panel";
 import { useLocalStorageJson } from "../../../hooks/useLocalStorageJson";
